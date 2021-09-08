8da0e683f79b9d7a8de2e06b19adb85e6234423c
陈爱国 <810158465@qq.com>
2021-09-08 16:35:04 +0800

    feat: 增加 props 类型限制属性

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,31 @@
 // eslint-disable-next-line no-unused-vars
 import * as React from 'react'
 
-declare class RcAudio extends React.Component<any, any> {
+export interface AudioProps {
+  src?: string | (() => string);
+  prefixCls?: string;
+  placement?: 'left' | 'right';
+  currentTime?: number;
+  volume?: number;
+  muted?: boolean;
+  showProgressBarInfo?: boolean;
+  cuePoints?: Array<{
+    time: number;
+    title: string;
+  }>;
+  showBufferProgress?: boolean;
+  onEnded?: (e: Event) => void;
+  onTimeUpdate?: (e: Event) => void;
+  onDurationChange?: (e: Event) => void;
+  onCuePoints?: (cuePoint: {
+    time: number;
+    title: string;
+    index: number;
+  }) => void;
+  renderCustom?: () => React.ReactElement;
+}
+
+declare class RcAudio extends React.Component<AudioProps, any> {
   onTimeUpdate: any;
   onPlay: any;
   onPause: any;
